7171c4f6e187ce34ae002746567cffd563ed54c1
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-04-23 21:50:39 -0600

    Modificacion para mejora de la leccion 17

--- pythonProject/Python desde Cero/curso17.Numero par o inpar.py
+++ pythonProject/Python desde Cero/curso17.Numero par o inpar.py
@@ -1,13 +1,12 @@
-#NUMERO PAR O INPAR
+# NUMERO PAR O INPAR
 
 print("*******************************************")
 print("*Programa que determina si un numero es par o inpar*")
 print("*******************************************")
 
-num1 = int(input ("Por favor ingresa un numero: "))
-
+num1 = int(input("Por favor ingresa un numero: "))
 
 if num1 % 2 == 0:
-    print("El numero ", num1, " es el que ingresaste es un numero par. \nFin.")
+    print("El numero ", num1, " es un numero par. \nFin.")
 else:
-    print("El numero ", num1, " es el que ingresaste es un numero inpar. \nFin.")    
+    print("El numero ", num1, " es un numero inpar. \nFin.")
