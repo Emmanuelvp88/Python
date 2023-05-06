beb3ada089ac96386ccaf6b6294ffc654457b07a
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-05-06 00:07:50 -0600

    Modificacion de nombres a todos

--- pythonProject/Python desde Cero/curso28 Metodo strip().py
+++ pythonProject/Python desde Cero/curso28 Metodo strip().py
@@ -3,10 +3,12 @@ print('\nAhora veremos el metodo "strip()"\n')
 # del inicio y el final de una cadena de texto, dentro de los parentecis del
 # "strip()" deveras meter los caranteres que deseas eliminar.
 
-print('Ejemplo 1')
-palabra = input('ingresa una cadena de texto: ')
-palabra = palabra.strip('n elm')
-print(palabra)
+print('Ejemplo 1: Si no le espesificamos al metodo '
+      'que caracter deseamos eliminar por defecro nos eliminara los espacios '
+      'en blanco del preincipio y del final\n')
+
+palabra = " Emmanuel "
+print(palabra.strip())
 
 print('\nEjemplo 2')
 # si el "strip" esta vacio solo eliminara los espacios en blanco, tabulaciones
