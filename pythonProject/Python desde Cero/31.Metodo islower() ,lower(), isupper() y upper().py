62177dc90355775dec56287ae0de6c1cce5cdcc1
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-05-07 21:53:58 -0600

    Subida de leccion 30 metodo isupper() y upper()

--- pythonProject/Python desde Cero/31.Metodo islower() ,lower(), isupper() y upper().py
+++ pythonProject/Python desde Cero/31.Metodo islower() ,lower(), isupper() y upper().py
@@ -1,18 +1,24 @@
 print(f'*************     Metodos islower() y lower()     *****************\n')
 print(f'El metodo islower() siver para comparar si una cadena de texto esta escrita con puras letras minusculas,'
-      f'si es verdadero nos regresara "true" de lo contrario "false"\n')
+      f'si es verdadero nos regresara "true" de lo contrario "false"')
 print('El metodo lower() en cambio es para convertir todas las letras minusculas de cualquier cadena de texto\n')
+print('*****************   Metodos isupper() Y upper()    ******************')
+print('El metodo isupper() es para comprobar si una cadena de caracteres contiene puras letras mayusculas,'
+      'si esto es verdadero nos regresara un "true" de lo contrario "false"')
+print('El metodo upper() se encarga de tranfromar una cadena de texto en puras letras MAYUSCULAS.\n')
 
 cadena = input(f'Por favor ingresa cualquier cadena de texto: ')
-cadena.islower();
-if cadena == True:
+
+if cadena.islower() == True:
     print(f'Tu cadena contiene puras minusculas.')
-else:
-    print(f'Tu cadena esta mezcada mayusculas y minusculas.')
-    respuesta = input(f'Quieres convertir tu cadena a puras minusculas?: (Y/N)')
-    if respuesta == "Y" or respuesta == "y":
-        print(f'Tu respuesta fue si, tu nueva cadena queda asi: {cadena.lower()}')
-    elif respuesta == "n" or respuesta == "N":
-        print(f'Tu respuesta fue No, tu nueva cadena queda asi:')
+elif cadena.isupper() == True:
+    print('Tu cadena contiene puras MAYUSCULAS.')
+elif cadena.islower() == False:
+    print(f'Tu cadena esta mezcada MAYUSCULAS y minusculas.\n')
+    respuesta = input(f'Selecciona una opcion:  convertir tu cadena a puras minusculas "m" o a Puras MAYUSCULAS "M"?: (m/M)')
+    if respuesta == "m":
+        print(f'Tu respuesta fue "m", tu cadena fue convertida a puras minusculas: {cadena.lower()}')
+    elif respuesta == "M":
+        print(f'Tu respuesta fue "M", tu cadena fue convertida en puras MAYUSCULAS:  {cadena.upper()}')
     else:
         print('Tu respuesta es invalida intenta denuevo.')
