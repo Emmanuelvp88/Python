1a2fdeef3a459405c0c56ac873520072221bfa76
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-05-07 23:06:51 -0600

    Subi leccion 32 metodo swapcase()

---
+++ pythonProject/Python desde Cero/32.Metodo swapcase.py
@@ -0,0 +1,8 @@
+print('***********************  Metodo swapcase() **************')
+print('El metodo swapcase() invierte las letras MAYUSCULAS en minusculas y las minusculas en MAYUSCULAS\n')
+
+cadena = input('Ingresa una cadena de texto que contenga minusculas y MAYUSCULAS: ')
+print(f'Esta es la cadena original: {cadena}')
+print(f'Esta es la cadena usando el metodo swapcase(): {cadena.swapcase()}')
+
+
