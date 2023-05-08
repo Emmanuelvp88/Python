af8e2e4c3aaa84f71bfd6c934fbf507b0373f3b2
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-05-07 22:44:13 -0600

    Cambios en leccion 26 metodo format()

--- pythonProject/Python desde Cero/26.Metodo format.py
+++ pythonProject/Python desde Cero/26.Metodo format.py
@@ -1,13 +1,15 @@
-print('Ahora veremos diferentes formas y metodos con lo que podemos concatenar y una de esos metodos es "format"')
+
+print('*************     Metodo format()     ************')
+print('Diferentes formas y metodos para concatenar ')
 
 nombre = 'Juan'
 edad = 22
 
-print('Ejemplo 1: con el signo +')
+print('Ejemplo 1: con el signo "+"')
 # Esta es la concatenacion tardicional con la estamos acostumbrados
 print('Hola ' + nombre + ' tu edad es de ', edad, ' años \n')
 
-print('Ejemplo 2')
+print('Ejemplo 2: Metodo format()')
 # En este ejemplo ponemos el orden de las variables dentro del format y asi es
 # como se mostraran en el print
 print('Hola {} tu edad es de {}'.format(nombre, edad), 'años \n')
