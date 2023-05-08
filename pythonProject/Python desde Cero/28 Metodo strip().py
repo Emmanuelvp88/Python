5f3bdcbec8ce68453f707ec9b07e7e35f7618647
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-05-07 23:11:30 -0600

    Mejoras en leccion 28 metodo strip()

--- pythonProject/Python desde Cero/28 Metodo strip().py
+++ pythonProject/Python desde Cero/28 Metodo strip().py
@@ -1,14 +1,14 @@
-print('\nAhora veremos el metodo "strip()"\n')
+print('\n***************    Metodo strip()    **********\n')
 # El metodo "strip()" se encarga de eliminar caracteres especificos del inicio y el final de una cadena de texto,
-# dentro de los parentecis del "strip()" deveras meter los caranteres que deseas eliminar.
+# dentro de los parentecis del "strip()" deveras poner los caranteres que deseas eliminar.
 
 print('Ejemplo 1: Si no le espesificamos al metodo '
       'que caracter deseamos eliminar por defecto nos eliminara los espacios '
       'en blanco del principio y del final\n')
 
 palabra = "   Emmanuel   "
-print("antes:", palabra)
-print("despues:",palabra.strip())
+print("Cadena original:", palabra)
+print("Cadena con el metodo strip():",palabra.strip())
 
 print('\nEjemplo 2: Al ingresar caracteres dentro del metodo "strip()" los caracteres se iran eliminando del principio y del final  de la cadena'
       'Con forme los valla encontrando los ira eliminando y saltara al siguinte caracter y asi sucesivamente hasta terminar con la cadena')
