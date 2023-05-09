71780d4a28099f086aaecb80d5fd7c0bc3c1d450
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-05-08 21:44:20 -0600

    Subida de leccion 34 metodos center(), ljust() y rjust()

---
+++ pythonProject/Python desde Cero/34.Metodo center(), ljust() y rjust().py
@@ -0,0 +1,6 @@
+print('*************   Metodo center()   ***********\n')
+print('El metodo center() se encarga de  centrar una cadena de texto, este metodo recibe para metros es decir que no \n'
+      'puede ir solos los pararentesis Ejemplo: cadena.center(10, "*") si cadena contiene 4 caracteres quedarian 3 a la derecha\n'
+      'y 3 a la izquierda pro que el primer parametroe es 10 y esto equivale a los espacios en total, el segundo parametro es para\n'
+      'llener los espacios en blanco que quedan a los lados, con cualquier caracter que uno indique, solo debe ponerse un caracter\n'
+      'entre comillas, en este caso es un parentesis asi qeu la cadena quedaria asi:***hola***')
