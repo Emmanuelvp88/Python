49127f8057ce420c53a35d708e1d259a2b777631
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-05-08 22:29:53 -0600

    Subida de leccion 34 metodos center(), ljust() y rjust()

--- pythonProject/Python desde Cero/34.Metodo center(), ljust() y rjust().py
+++ pythonProject/Python desde Cero/34.Metodo center(), ljust() y rjust().py
@@ -3,4 +3,13 @@ print('El metodo center() se encarga de  centrar una cadena de texto, este metod
       'puede ir solos los pararentesis Ejemplo: cadena.center(10, "*") si cadena contiene 4 caracteres quedarian 3 a la derecha\n'
       'y 3 a la izquierda pro que el primer parametroe es 10 y esto equivale a los espacios en total, el segundo parametro es para\n'
       'llener los espacios en blanco que quedan a los lados, con cualquier caracter que uno indique, solo debe ponerse un caracter\n'
-      'entre comillas, en este caso es un parentesis asi qeu la cadena quedaria asi:***hola***')
+      'entre comillas, en este caso es un parentesis asi qeu la cadena quedaria asi:***hola***\n')
+
+cadena = input('Ingresa una cadena: ')
+caracter = input('Ingresa el caracter que deseas tener a los lados, si no, puedes dejarlos en blanco: ')
+espacios = int(input('cuantos espacios quieres a los lados?: '))
+espacios = len(cadena) + espacios * 2;
+if len(caracter) == 0:
+      caracter = " "
+
+print(cadena.center(espacios, caracter))
