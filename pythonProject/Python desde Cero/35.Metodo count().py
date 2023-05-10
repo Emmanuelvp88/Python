8a976104e99cf428b039975ce27a0fb757405138
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-05-09 22:05:49 -0600

    Subuda de leccion 35 metodo count()

---
+++ pythonProject/Python desde Cero/35.Metodo count().py
@@ -0,0 +1,14 @@
+print('*************  Metodo count()   *************\n')
+print('el metodo conunt() nos permite contar cuantos caracteres tienen una cadena de texto\n'
+      'si no le pasamos parametros Ejemplo: dadena.count("") solo contara cuantos caracteres tiene una cadena de texto\n'
+      'En este otro Eejemplo: cadena.count("m") nos contara cuantas "m" existen en la cadena de etexto,\n'
+      'el este  otro Ejemplo: cadena.count("ma", 3, 9) cuando le pasamos 3 parametros, buscara la palabra o caracter\n'
+      'que le pasamos como primer parametro y los otros dos de tipo "int" son el inicio y el final de \n'
+      'de busqueda en la cadena.\n')
+
+cadena = input('ingresa una cadena de texto: ')
+caracter = input('Que caracter quieres contar: ')
+comienzo = int(input('De donde quieres que empieze: '))
+final = int(input('Donde quieres que termine: '))
+print(f'El caracter o palabra "{caracter}" se encuentra {cadena.count(caracter, comienzo, final)} veces en la cadena que ingresaste.')
+
