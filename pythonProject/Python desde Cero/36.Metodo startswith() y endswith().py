7e48b5a47ebc4925f10444b1941cb57e28195650
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-05-12 22:48:59 -0600

    Subida de leccion 36 metodo startswith()

--- pythonProject/Python desde Cero/36.Metodo startswith() y endswith().py
+++ pythonProject/Python desde Cero/36.Metodo startswith() y endswith().py
@@ -2,4 +2,10 @@ print('*********************  Metodo startswith() y endswith()   ***************
 print('El metodo stratswith() busca y compara si ciertos carasteres se encuentran'
       'al principio en una cadena de carasteres, solo funciona con parametros, Ejemplo: cadena.startswith("D")'
       'Si lo manejamos con 3 parametros Ejemplo: cadena.startwith("D", "int" "int") donde el primer parametro '
-      'nos indica el caracter que vamos a buscar y los parametro 2 y 3 nos indican de donde hasta donde buscar en la cadena')
+      'nos indica el caracter o palabra a buscar y los parametro 2 y 3 nos indican donde  vamos a empezar '
+      'y hasta donde vamos a terminar en la cadena\n')
+
+cadena = 'Diana lava la ropa'
+print('******    Metodo startswith()    *********')
+print(f'La cadena "{cadena}" ¿Empieza con la letra "D"?: {cadena.startswith("D")}')
+print(f'La cadena "{cadena}" ¿Empieza con la palabra "Diana"?: {cadena.startswith("Diana")}')
