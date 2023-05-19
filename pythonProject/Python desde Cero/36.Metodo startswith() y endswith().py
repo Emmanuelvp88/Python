cb0cbbceebaa85374926daa44ad00dd5d2903e41
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-05-18 22:50:48 -0600

    Mejora de leccion 36 metodo startswith()

--- pythonProject/Python desde Cero/36.Metodo startswith() y endswith().py
+++ pythonProject/Python desde Cero/36.Metodo startswith() y endswith().py
@@ -2,14 +2,14 @@ print('*********************  Metodo startswith() y endswith()   ***************
 print('El metodo stratswith() busca y compara ciertos carasteres se encuentren\n'
       'al principio en una cadena, solo funciona con parametros, Ejemplo: cadena.startswith("D")\n'
       'Si lo manejamos con 3 parametros Ejemplo: cadena.startwith("D", "int" "int") donde el primer parametro\n'
-      'nos indica el caracter o palabra a buscar y los parametro 2 y 3 nos indican donde  vamos a empezar '
+      'nos indica el caracter o palabra a buscar y los parametro 2 y 3 nos indican donde  vamos a empezar\n'
       'y hasta donde vamos a terminar en la cadena\n')
 
 cadena = 'Diana lava la ropa'
-print('******    Metodo startswith()    *********')
+print('******    Metodo startswith()    *********\n')
 print(f'La cadena "{cadena}" ¿Empieza con la letra "D"?: {cadena.startswith("D")}')
 print(f'La cadena "{cadena}" ¿Empieza con la palabra "Diana"?: {cadena.startswith("Diana")}')
 
-print('\n*********     Metodo endswith()   **********')
+print('\n*********     Metodo endswith()   **********\n')
 print(f'La cadena "{cadena}" ¿termina con la letra "p"? {cadena.endswith("p")}')
 print(f'La cadena "{cadena}" ¿termina con la palabra "ropa"? {cadena.endswith("ropa")}')
