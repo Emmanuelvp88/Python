e776c4b03c63568817ef80c800ae254c7a5cf091
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-05-20 19:55:11 -0600

    Mejoras al archivo init

---
+++ pythonProject/Kuepa/Programacion con Python/__init__.py
@@ -0,0 +1,7 @@
+lista = ['stephen.marquard@uct.ac.za', 'Saturday', 'Jan', 5, '09:14.16', 2008]
+validacion = input('Escriba el nombre del archivo correcto: ')
+
+if validacion == "Historial.txt":
+    for i in lista:
+        print(lista)
+
