d7395e008a03a3e1cba73903f837df528935570b
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-05-21 23:25:20 -0600

    mejoras en leccion 37

--- pythonProject/Python desde Cero/37.substring.py
+++ pythonProject/Python desde Cero/37.substring.py
@@ -2,11 +2,12 @@ print('**************   Substring    ***********')
 print('\n Un Substring o subcadena es una sucecion de caracteres dentro de una cadena\n'
       'y con las Substring podemos obtener una parte de una cadena o solo un caracter si asi lo deseamos\n'
       'Ejemplo: prueba = cadena[0:3:2] el primer parametro es el inicio del indice, el segundo es donde\n'
-      'terminara el indice, el tercero es los caracteres que se va saltar sin agregarlos.')
+      'terminara el indice, el tercero son los caracteres que quieres que se salte sin agregarlos.\n')
 
 cadena = 'Aprendiendo Python desde cero'
-print(f'Esta es la cadena original: {cadena}')
-
+print(f'Esta es la cadena original: {cadena}\n')
+print(f'Mostramos del 0 al 10: {cadena[0:10]}\n')
+print(f'Mostramos los caracteres saltando cada 2: {cadena[0:100:2]}')
 
 
 
