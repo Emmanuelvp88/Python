089cf83befff9296c471e6ef4b6cf3cfecce2125
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-05-22 22:34:59 -0600

    Mejora leccion 30

--- pythonProject/Python desde Cero/30.Metodo istitle() y title().py
+++ pythonProject/Python desde Cero/30.Metodo istitle() y title().py
@@ -4,12 +4,17 @@ lastName = input('ingresa tu apellido: ')
 nombre = f'{firstName} {lastName}'
 
 print(f"Cadena original: {nombre}\n");
-print(f'Cadena con metodo istitle(): {nombre.istitle()}');
-print("nos regreso false proque la cadena esta escrito incoreectamente, tiene mezcaldas mayusculas y minisculas.\n")
-print(f'Cadena con el metodo title(): {nombre.title()} ');
-print(
-    f'El metodo title() se encarga de corregir la cadena poniendo mayusculas al principio y las demas minusculas.')
+print(f'Metodo istitle(): {nombre.istitle()}');
+if nombre.istitle() == False:
+    print("nos regreso false proque la cadena esta escrito incoreectamente, tiene mezcaldas mayusculas y minisculas.\n")
+else:
+    print('La cadena es semanticamnete correcta')
 
-# El metodo istitle() se encarga de revisar con anticipacion la cadena de texto, mandandonos un "true o false"
-# si la cadena esta escrita correctamente respetando mayusculas y minusculas con respecto asu correcta semantica,
-# si esta escrita correctamente nos mandara como resultado un "true" y si esta mal escrita "false"
+print(f'Cadena corregida con el metodo title(): {nombre.title()} ');
+print(f'El metodo title() se encarga de corregir la cadena poniendo mayusculas al principio de cada palbra\n'
+      f' y las demas en minusculas y si encuantra una mayuscula despues de la primera letra la corrige convirtiendola en minuscula .')
+
+# El metodo istitle() se encarga de revisar si una cadena de texto se encuentra bien escrita semanticamente
+# si es asi nos mandara un "true" o  un "false" si noe s asi
+# la manera correcta seria en principio de cada palabra lleva mayuscula "true"
+# minusculas al principio de cada palabra  y mayusculas entrecaladas
