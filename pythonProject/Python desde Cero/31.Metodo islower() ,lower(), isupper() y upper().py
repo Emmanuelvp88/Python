e537f71920941c9726d70d72b39e72d9ee225db3
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-05-22 21:54:30 -0600

    Mejora leccion 31

--- pythonProject/Python desde Cero/31.Metodo islower() ,lower(), isupper() y upper().py
+++ pythonProject/Python desde Cero/31.Metodo islower() ,lower(), isupper() y upper().py
@@ -1,12 +1,12 @@
 print(f'*************     Metodos islower() y lower()     *****************\n')
-print(f'El metodo islower() siver para comparar si una cadena de texto esta escrita con puras letras minusculas,'
-      f'si es verdadero nos regresara "true" de lo contrario "false"')
-print('El metodo lower() en cambio es para convertir todas las letras minusculas de cualquier cadena de texto\n')
+print(f'El metodo islower() siver para verificar si una cadena de texto esta escrita con puras letras minusculas,'
+      f'si esto es verdadero nos regresara "true" de lo contrario "false"')
+print('El metodo lower() en cambio se encarga de convertir  directamente en letras minusculas cualquier cadena de texto\n')
 
 print('*****************   Metodos isupper() Y upper()    ******************')
-print('El metodo isupper() es para comprobar si una cadena de caracteres contiene puras letras mayusculas,'
+print('El metodo isupper() verifica si una cadena contiene puras letras MAYUSCULAS,'
       'si esto es verdadero nos regresara un "true" de lo contrario "false"')
-print('El metodo upper() se encarga de tranfromar una cadena de texto en puras letras MAYUSCULAS.\n')
+print('El metodo upper() se encarga directamente de tranformar  a MAYUSCULAS una cadena de texto.\n')
 
 cadena = input(f'Por favor ingresa cualquier cadena de texto: ')
 
