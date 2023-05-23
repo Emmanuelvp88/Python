fc988c78ff315f9027fb0615ab223fea3572f1cd
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-05-22 21:47:58 -0600

    Mejora leccion 30

--- pythonProject/Python desde Cero/30.Metodo istitle() y title().py
+++ pythonProject/Python desde Cero/30.Metodo istitle() y title().py
@@ -8,7 +8,7 @@ print(f'Cadena con metodo istitle(): {nombre.istitle()}');
 print("nos regreso false proque la cadena esta escrito incoreectamente, tiene mezcaldas mayusculas y minisculas.\n")
 print(f'Cadena con el metodo title(): {nombre.title()} ');
 print(
-    f'El metodo title() se encarga de componer la cadena de la manera correcta, mayusculas al principio y las demas minusculas.')
+    f'El metodo title() se encarga de corregir la cadena poniendo mayusculas al principio y las demas minusculas.')
 
 # El metodo istitle() se encarga de revisar con anticipacion la cadena de texto, mandandonos un "true o false"
 # si la cadena esta escrita correctamente respetando mayusculas y minusculas con respecto asu correcta semantica,
