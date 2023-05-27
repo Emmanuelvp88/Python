0d267e06a814f1e9fd22cd31de7b44ec883adfc2
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-05-27 16:26:57 -0600

    mejorando la leccion 38 x2

--- pythonProject/Python desde Cero/38.Ejercicio(Eliminar una palabra).py
+++ pythonProject/Python desde Cero/38.Ejercicio(Eliminar una palabra).py
@@ -2,5 +2,5 @@ cadena = input('Ingresa una cadena de texto: ')
 frase = input('Ingresa la frase que deseas eliminar de la cadena: ')
 indice = cadena.find(frase)
 
-fraseRecortada = cadena[ :indice] + cadena[len(frase): ]
+fraseRecortada = cadena[:indice] + cadena[indice + len(frase)+1:]
 print(fraseRecortada)
