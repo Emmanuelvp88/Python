58eec52a765b936b1376d57ae6d4cddaf7e50a35
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-05-27 14:05:46 -0600

    mejoras en code leccion 21

--- pythonProject/Python desde Cero/21.Parametro end Y sep.py
+++ pythonProject/Python desde Cero/21.Parametro end Y sep.py
@@ -1,25 +1,23 @@
 print('Haora veremos los parametros "end" y "sep".')
 
 print('\nParametro "end"')
-# El parametro "end" se encarga de Unir unas cadenas de caracteres aun que haya
-# un salto de linea, cabe mencionar que solo agregara la sigiente linea de codigo
-# y la unira en una sola linea con la anterior, ose que la anterior linea sera una
-# sola con la sigiente linea.
+# El parametro "end" se encarga de unir cadenas eliminando saltos de linea
+# NOTA: solo agregara la sigiente linea de codigo y la unira con la anterior en una sola linea
 
 print("Esto es un ", end=" ")
 print("Ejemplo de lo que", end=" ")
 print("Hace el parametro end.")
 
 print('\nParametro "sep"')
-# El parametro "sep" se encarga de las separaciones  de unas cadenas de caracteres
-# dentro de un print, ya sea eliminar los espacion o agragando algo entre los espacio de lacadena.
+# El parametro "sep" se encarga de eliminar los espacion o agragar algo
+# entre los espacio de una cadena.
 
 # por default Python nos pone un espacio entre cada caracter.
-print("1", "2", "3", "4", "5")
+print('cadena original: ', "1", "2", "3", "4", "5")
 # pero lo podemos eliminar con el parametro "sep"
-print("1", "2", "3", "4", "5", sep="")
-# o complemetarlo con lo que queramos ya sea una "Coma"
-print("1", "2", "3", "4", "5", sep=",")
+print('quitando los espacios: ' + "1", "2", "3", "4", "5", sep="")
+# o complemetarlo con cualquier caracter que queramos en este caso usaremos una coma
+print("Separacion con coma; 1" , "2" , "3" , "4" , "5", sep=",")
 # un "Gion"
 print("1", "2", "3", "4", "5", sep="-")
 # o un signo
