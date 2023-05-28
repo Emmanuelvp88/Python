c960da4cb6a0f29daf6c102056966568acd6720e
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-05-27 21:33:34 -0600

    agrego un xtraoficial 31 metodo sort()

--- pythonProject/Python desde Cero/31.Extraoficial Metodo sort().py
+++ pythonProject/Python desde Cero/31.Extraoficial Metodo sort().py
@@ -5,3 +5,9 @@ lista1 = ["Emmanuel", "Juan", "Carlos", "Ruben"]
 print('Lista original: ', lista1)
 lista1.sort()# NO se puede poner este metodo dentro del mismo print
 print(f'lista ordenada: {lista1}')
+
+print('**********   Metodos min(), max(), sum()   **********')
+lista2 = [3,5,23,45,23]
+print(min(lista2))
+print(max(lista2))
+print(sum(lista2))
