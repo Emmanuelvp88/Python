9eaf667a2d36be3a2581b371ebfb61f435396e9c
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-05-28 15:08:16 -0600

    Copiando lo de la prueva hacia la actividad

--- pythonProject/Kuepa/Programacion con Python/Actividad1Sem2.py
+++ pythonProject/Kuepa/Programacion con Python/Actividad1Sem2.py
@@ -1,15 +1,16 @@
-with open("Historial.txt") as archivo:
-    for i in archivo:
-        if i.startswith("From "):
-            print(i.rstrip())
-#diccionario = dict()
-lista1 = archivo.split()
-print(lista1)
-#for i in lista1:
- #   if i not in diccionario:
-  #      diccionario[i] = 1
-   # else:
-    #    diccionario[i] = diccionario[i] + 1
-#print(diccionario)
-
+nombreArchivo = input('Ingrse el nombre del archivo: ')
+try:
+     archivo = open(nombreArchivo)
+except:
+    print(f'el archivo "{nombreArchivo}" es invalido')
+    quit()
+dicc = dict()#creamos un diccionario tambien se puede de ssta otra manera "Dicci = {}"
+for lineas in archivo:#recoremos el archivo
+    if lineas.startswith("From "):#seleccionamos las lineas que empizan con from
+        listaLineas = lineas.split()#Almacenamos las lineas en una lista
+        dia = listaLineas[2]#Guardamos el elemento 2 de la lista en una variable
+        listaDia = dia.split()# y Guardamos esa var en una lista
+        for dias in listaDia:# recorremos esa lista para guardarla en un diccionario y hacer el conteo
+            dicc[dias] = dicc.get(dias, 0) + 1
+print(dicc)
 
