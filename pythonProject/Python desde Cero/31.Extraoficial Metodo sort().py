0bafdad41bab9abbd034fd1b013be5c09fba7912
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2023-05-27 20:59:36 -0600

    agrego un xtraoficial 31 metodo sort()

---
+++ pythonProject/Python desde Cero/31.Extraoficial Metodo sort().py
@@ -0,0 +1,7 @@
+print('***********   Metodo sort()  ***********')
+#sort() se encarga de ordenar alfabeticamente los elementos de una lista
+
+lista1 = ["Emmanuel", "Juan", "Carlos", "Ruben"]
+print('Lista original: ', lista1)
+lista1.sort()# NO se puede poner este metodo dentro del mismo print
+print(f'lista ordenada: {lista1}')
